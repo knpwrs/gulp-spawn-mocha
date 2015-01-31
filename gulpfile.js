3f1dfb4160b9cc3c0f1a2eadd393c393f9ce6e94
Kenneth Powers <ken@kenpowers.net>
2015-01-31 18:16:09 -0500

    Remove throw up from gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,19 @@
+const DEBUG = process.env.NODE_ENV === 'debug',
+      CI = process.env.CI === 'true';
+
 var gulp = require('gulp'),
     mocha = require('./lib');
 
 gulp.task('test', function () {
-  return test('spec').on('error', function (e) {
-    throw e;
-  });
+  return gulp.src(['test/*.test.js'], {read: false})
+    .pipe(mocha({
+      debugBrk: DEBUG,
+      r: 'test/setup.js',
+      R: CI ? 'spec' : 'nyan',
+      istanbul: !DEBUG
+    }));
 });
 
-gulp.task('default', function () {
-  gulp.watch('{lib,test}/*', test);
-  test();
+gulp.task('default', ['test'], function () {
+  gulp.watch('{lib,test}/*', ['test']);
 });
-
-function test(reporter) {
-  var isDebug = process.env.NODE_ENV === 'debug';
-  return gulp.src(['test/*.test.js'], {read: false}).pipe(mocha({
-    debugBrk: isDebug,
-    r: 'test/setup.js',
-    R: typeof reporter === 'string' ? reporter : 'nyan',
-    istanbul: !isDebug
-  })).on('error', console.warn.bind(console));
-}
