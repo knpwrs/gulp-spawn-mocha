28abfbd5409223d32c392eb0673ea5627d4cfb87
unbug <tidelgl@gmail.com>
2015-05-14 09:11:06 +0800

    outstream

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,19 @@ const DEBUG = process.env.NODE_ENV === 'debug',
       CI = process.env.CI === 'true';
 
 var gulp = require('gulp'),
-    mocha = require('./lib');
+    mocha = require('./lib'),
+    through2 = require('through2');
 
 gulp.task('test', function () {
-  return gulp.src(['test/*.test.js'], {read: false})
+  return gulp.src(['test/*.test.js'], {read: 1})
     .pipe(mocha({
       debugBrk: DEBUG,
       r: 'test/setup.js',
       R: CI ? 'spec' : 'nyan',
-      istanbul: !DEBUG
-    }));
+      istanbul: !DEBUG,
+          outstream: 'outstream.html'
+    }))
+      .pipe(gulp.dest('report.html'));
 });
 
 gulp.task('default', ['test'], function () {
---
+++ report.html/index.test.js
@@ -0,0 +1,98 @@
+describe('gulp-spawn-mocha tests', function () {
+  var mocha = require('../lib'),
+      through = require('through'),
+      proc = require('child_process'),
+      join = require('path').join,
+      PluginError = require('gulp-util').PluginError;
+
+  beforeEach(function () {
+    sinon.stub(proc, 'fork');
+    this.childOn = sinon.stub();
+    proc.fork.returns({
+      stdout: sinon.stub(through()),
+      stderr: sinon.stub(through()),
+      on: this.childOn
+    });
+  });
+
+  afterEach(function () {
+    // Common assertions
+    proc.fork.should.be.calledOnce;
+    proc.fork.should.be.calledWith(sinon.match.string, sinon.match.array);
+    // Restore fork functionality
+    proc.fork.restore();
+  });
+
+  describe('main', function () {
+    it('should buffer filenames and pass them to mocha', function () {
+      var stream = this.stream = mocha();
+      var paths = ['foo', 'bar', 'baz'];
+      paths.forEach(function (path) {
+        stream.write({path: path});
+      });
+      stream._files.should.deep.equal(paths);
+      proc.fork.should.not.be.called;
+      stream.end();
+      proc.fork.should.be.calledWith(sinon.match.string, this.stream._files);
+    });
+
+    it('should default to proper binary', function () {
+      var bin = join(require.resolve('mocha'), '..', 'bin', 'mocha');
+      var stream = this.stream = mocha();
+      stream.end();
+      proc.fork.should.be.calledWith(bin, []);
+    });
+
+
+    it('should allow for a custom mocha binary', function () {
+      var stream = this.stream = mocha({bin: 'foo mocha'});
+      stream.end();
+      proc.fork.should.be.calledWith('foo mocha', []);
+    });
+
+    it('should allow for a custom environment', function () {
+      var stream = this.stream = mocha({env: {'FOO' : 'BAR'}});
+      stream.end();
+      proc.fork.should.be.calledWith(sinon.match.any, sinon.match.any, sinon.match({env: {'FOO' : 'BAR'}}));
+    });
+
+    it('should pass arguments to mocha, properly prefixing, dashifying, and ignoring', function () {
+      var stream = this.stream = mocha({foo: 'bar', b: ['oof', 'rab'], debugBrk: true, isAString: true, R: 'spec', S: true, T: false, U: null, V: undefined});
+      stream.end();
+      proc.fork.should.be.calledWith(sinon.match.string, ['--foo', 'bar', '-b', 'oof', '-b', 'rab', '--debug-brk', '--is-a-string', '-R', 'spec', '-S']);
+    });
+
+    it('should handle errors from mocha', function () {
+      this.childOn.yields(-1);
+      var stream = this.stream = mocha();
+      sinon.stub(stream, 'emit');
+      stream.emit.withArgs('error').returns();
+      stream.end();
+      this.childOn.should.be.calledTwice;
+      stream.emit.should.be.calledWith('error', sinon.match.instanceOf(PluginError));
+    });
+  });
+
+  describe('istanbul functionality', function () {
+    var bin = join(require.resolve('istanbul'), '..', require('istanbul/package.json').bin.istanbul);
+    var mbin = join(require.resolve('mocha'), '..', 'bin', '_mocha');
+
+    it('should properly call istanbul with no arguments', function () {
+      var stream = this.stream = mocha({istanbul: true});
+      stream.end();
+      proc.fork.should.be.calledWith(bin, ['cover', '--', mbin]);
+    });
+
+    it('should properly call istanbul with one more more arguments', function () {
+      var stream = this.stream = mocha({istanbul: {verbose: true, print: 'detail'}});
+      stream.end();
+      proc.fork.should.be.calledWith(bin, ['cover', '--verbose', '--print', 'detail', '--', mbin]);
+    });
+
+    it('can use a custom binary', function () {
+      var stream = this.stream = mocha({istanbul: {bin: 'isparta'}});
+      stream.end();
+      proc.fork.should.be.calledWith('isparta', ['cover', '--', mbin]);
+    });
+  });
+});
