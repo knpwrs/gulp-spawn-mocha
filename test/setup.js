4f1695b8883fb3fda8ea0028b1db185ffb74d0b8
raccoon-lee <raccoon@hahow.in>
2017-12-25 15:23:13 +0800

    style(project): apply eslint-hahow

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,10 @@
+'use strict'
+
 // Setup chai
-var chai = require('chai');
-global.expect = chai.expect;
-chai.should();
-chai.use(require('sinon-chai'));
+let chai = require('chai')
+global.expect = chai.expect
+chai.should()
+chai.use(require('sinon-chai'))
 
 // Setup sinon
-global.sinon = require('sinon');
+global.sinon = require('sinon')
